d14a3d8c3395c85e2b7e39341b9800edc11d4471
johannesvollmer <johannes.boenki@t-online.de>
2020-05-05 15:10:41 +0200

    80% done

--- src/Main.elm
+++ src/Main.elm
@@ -1,3 +1,5 @@
+module Main exposing (..)
+
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
@@ -6,7 +8,12 @@ import Phrase exposing (Phrase, phraseSet)
 import Random.List
 import Random exposing (Generator)
 import Json.Decode exposing (Decoder, field, string, int, float)
+import Json.Encode as Encode
 import Random.Extra exposing (combine)
+import File.Download
+import Levenshtein
+import Task
+import Browser.Dom
 
 main =
   Browser.element
@@ -16,22 +23,25 @@ main =
     , view = view
     }
 
-blockCount = 4
-timepoints = [0.0, 0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9, 1.0] |> List.repeat blockCount |> List.concat
-blockSize = List.length timepoints // blockCount
+blockCount = 3
+timepointsamples = [0.0, 0.5, 1.0] -- [0.0, 0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9, 1.0]
+
+blockSize = List.length timepointsamples
 
 type Message
-  = Randomize (List RandomPhrase)
+  = None
+  | Randomize (List RandomPhrase)
   | Next Time
   | Type Time String
+  | Suggestion String Time
+  | Download
 
 type alias Time = Float
 
 type alias Model =
-  { events: List Message
+  { events: List Encode.Value
   , phrases: List RandomPhrase -- drained from front, randomized order
   , phraseIndex: Int
-  , blockIndex: Int
   , mode: Mode
   }
 
@@ -46,25 +56,23 @@ initial =
   { events = [] -- reverse actually
   , phrases = [] -- to be filled with randomized data
   , phraseIndex = 0
-  , blockIndex = 0
   , mode = Ready
   }
   
 randomize: Generator (List RandomPhrase)
 randomize = 
   let 
-    shuffleSuggestions: Phrase -> Generator Phrase
     shuffleSuggestions phrase = 
       Random.List.shuffle (phrase.target :: phrase.variants)
         |> Random.map (List.take 3 >> Phrase phrase.target)
-
-    phrases: Generator (List Phrase)
+        
+    shuffledTimepoints = timepointsamples |> List.repeat blockCount |> List.map Random.List.shuffle |> combine
     phrases = phraseSet |> List.map shuffleSuggestions |> combine
 
   in Random.map2
     (List.map2 (\phrase point -> RandomPhrase phrase.target phrase.variants point))
     (Random.andThen Random.List.shuffle phrases)
-    (Random.List.shuffle timepoints)
+    (Random.map List.concat shuffledTimepoints)
 
 
 
@@ -74,23 +82,33 @@ type Mode
   | Presenting
   | Typing String
   
-  | Download Results
-  
 -- contains downloadable blob url
 type alias Results = String
 
 update : Message -> Model -> (Model, Cmd Message)
 update event model = 
   let
-    recordEvent = { model | events = event :: model.events }
+    records = case event of
+      Randomize data -> Encode.object [ ("phrases", Encode.list phraseToJson data) ] :: model.events
+      Next time -> Encode.object [ ("time", Encode.float time), ("next", Encode.null) ] :: model.events
+      Type time text -> Encode.object [ ("time", Encode.float time), ("text", Encode.string text) ] :: model.events
+      Suggestion phrase time -> Encode.object [ ("time", Encode.float time), ("suggestion", Encode.string phrase) ] :: model.events
+      _ -> model.events
+
+    phraseToJson phrase = Encode.object 
+      [ ("target", Encode.string phrase.target)
+      , ("threshold", Encode.float phrase.percentage)
+      , ("suggestions", Encode.list Encode.string phrase.suggestions)
+      ]
+
+    recordEvent = { model | events = records }
     phrasesCompleted = remainderBy blockSize (model.phraseIndex + 1) == 0
-    blocksCompleted = model.phraseIndex // blockSize == 3
 
     nextPhrase = 
       { recordEvent
       | phrases = List.tail recordEvent.phrases |> Maybe.withDefault []
       , phraseIndex = recordEvent.phraseIndex + 1
-      , mode = Presenting 
+      , mode = Presenting
       }
 
     result = case event of
@@ -98,52 +116,98 @@ update event model =
       Next _ -> case model.mode of
         Ready -> { recordEvent | mode = Presenting }
         Presenting -> { recordEvent | mode = Typing "" }
-        Typing _ -> if phrasesCompleted then { nextPhrase | mode = Ready } else nextPhrase
-        Download _ -> model
+        Typing _ -> 
+          if phrasesCompleted then { nextPhrase | mode = Ready } 
+          else nextPhrase
 
-      Type time newValue -> case model.mode of
-        Typing oldValue -> { recordEvent | mode = Typing newValue }
+      Type _ newValue -> case model.mode of
+        Typing _ -> { recordEvent | mode = Typing newValue }
         _ -> model
 
-  in (result, Cmd.none)
+      Suggestion _ _ -> 
+          if phrasesCompleted then { nextPhrase | mode = Ready } 
+          else nextPhrase
+
+      _ -> model
+
+    command = case event of
+      Download -> File.Download.string "messungen.json" "text/json" 
+        (List.reverse model.events |> Encode.list identity |> Encode.encode 4)
+        
+      Next _ -> case model.mode of
+        Presenting -> Task.attempt (always None) (Browser.Dom.focus "phrase-textbox")
+        _ -> Cmd.none
+
+      _ -> Cmd.none
+
+  in (result, command)
 
 
 view : Model -> Html Message
 view model =
   let 
-    contents = case (model.mode, model.phrases) of 
-      (Ready, _) -> div [] 
-        [ text "Bereit für den nächsten Schritt."
-        , Html.button [ recordClick Next ] [ text "Los geht's!" ] 
-        ]
+    phraseIndex = remainderBy blockSize model.phraseIndex + 1
+    progress = "(" ++ String.fromInt phraseIndex ++ "/" ++ String.fromInt blockSize ++ ")"
 
-      (Presenting, phrase :: _) -> div [] 
-        [ text ("Prägen Sie sich folgende Phrase ein: `" ++ phrase.target ++ "`")
-        , Html.button [ recordClick Next ] [ text "Starten" ]
-        ]
+    contents = case model.phrases of
+      [] -> 
+          [ Html.p [] [text "Schick mir bitte folgende Datei: "]
+          , Html.button [ onClick Download ] [ text "Herunterladen" ] 
+          ]
 
-      (Typing transcription, phrase :: _) -> div []
-        [ Html.input [ Html.Attributes.value transcription, recordTyping ] [] -- TODO focus?
-        , Html.button [ recordClick Next ] [ text "Fertig!" ] 
-        ]
+      phrase :: _ -> case model.mode of 
+        Ready -> 
+          [ Html.p [] [text "Entspanne dich und warte auf das Ok von Johannes."]
+          , Html.button [ recordClick Next, Html.Attributes.class "next" ] [ text "Experiment starten" ] 
+          ]
 
-      (Download download, _) -> div []
-        [ text ("Schick mir bitte folgende Nachricht: " ++ download) 
-        ]
+        Presenting ->
+          [ Html.h2 [] [text ("Phrase " ++ progress ++ "")]
+          , Html.p [ Html.Attributes.class "phrase" ] [text phrase.target]
+          , Html.button [ recordClick Next, Html.Attributes.class "next" ] [ text "Zeitmessung Starten!" ]
+          ]
 
-      _ -> text "Fertig, yay!"
+        Typing transcription -> 
+          let
 
-  in
-    div []
-      [ contents ]
+            requiredLetters = phrase.percentage * (String.length phrase.target |> toFloat) |> floor
+            requiredPart = String.left requiredLetters phrase.target
+            transcribedPart = String.left requiredLetters transcription
+            showSuggestions = 
+              if phrase.percentage == 0.0 then True
+              else if phrase.percentage == 1.0 then False
+              else String.length transcription >= requiredLetters && 
+                (Levenshtein.distance requiredPart transcribedPart |> toFloat) <= (String.length phrase.target |> toFloat) * 0.4 -- The standard error rate for the QWERTY is 12.7%
+
+            suggestion prediction = Html.button 
+              [ recordClick (Suggestion prediction), Html.Attributes.disabled (not showSuggestions), Html.Attributes.class "suggestion" ] 
+              [ text (if showSuggestions then prediction else "...") ]
+
+            suggestions = phrase.suggestions |> List.map suggestion
+
+            pre = 
+              [ Html.h2 [] [text ("Phrase " ++ progress)]
+              , Html.input 
+                [ Html.Attributes.value transcription
+                , Html.Attributes.class "phrase"
+                , Html.Attributes.id "phrase-textbox"
+                , recordTyping
+                , Html.Events.preventDefaultOn "paste" (Json.Decode.succeed (None, True))
+                ] [] 
+              ]
+
+            post = [ Html.button [ recordClick Next, Html.Attributes.class "next" ] [ text "Zeitmessung beenden!" ] ]
+            
+          in List.concat [ pre, suggestions, post ]
+
+  in div [ Html.Attributes.id "content" ] contents
 
 
 recordTyping : Html.Attribute Message
-recordTyping = 
-  Html.Events.on "input" (Json.Decode.map2 Type timeStamp Html.Events.targetValue) 
+recordTyping = Html.Events.on "input" (Json.Decode.map2 Type timeStamp Html.Events.targetValue) 
 
 recordClick : (Float -> Message) -> Html.Attribute Message
 recordClick message = Html.Events.on "click" (timeStamp |> Json.Decode.map message)
 
-
 timeStamp = field "timeStamp" float
+
